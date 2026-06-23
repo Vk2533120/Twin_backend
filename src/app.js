8f1cd9c3f61b13d7a7644ede7f26af8b40d81192
Vk2533120 <vk2533120@gmail.com>
2026-06-23 14:05:33 +0530

    new url added

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ const allowedOrigins = [
     'http://localhost:5173', 
     'http://127.0.0.1:5500', 
     'http://localhost:5500',
-    'https://ai-twin-git-1064507568780.asia-sout  h1.run.app/' // <-- Your new live frontend domain!
+    'https://ai-twin-git-1064507568780.asia-sout  h1.run.app/',
+    'https://www.twinn.live/' // <-- Your new live frontend domain!
   ];
 app.use(cors({
   origin: function (origin, callback) {
